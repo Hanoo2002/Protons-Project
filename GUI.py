1a82aa7c9c0fca5dda20c6107d27f1c1626d3a1e
hanoo2002 <hana.ghanem@gmail.com>
2017-08-21 01:08:06 +0200

    commit 5

--- GUI.py
+++ GUI.py
@@ -26,9 +26,44 @@ for frame in (Home, AddActivity, AddCategory, EditActivity, EditCategory, Calend
 ########################################################################################################################
 # Home Frame
 
+class Add:
+    def __init__(self, parent):
+        top = self.top = Toplevel(parent,bg='blue')
+
+        Label(top, fg='white',bg='blue' ,text="Add").pack()
+
+        Label(top,fg='white',bg='blue' , text='What do you want to add?')
+
+        cat = Button(top,fg='white',bg='blue' , text="Category", command=lambda: raise_frame(AddCategory))
+        cat.pack(pady=5, padx=5, side=LEFT)
+
+        act = Button(top,fg='white',bg='blue' , text="Activity", command=lambda: raise_frame(AddActivity))
+        act.pack(pady=5, padx=5, side=LEFT)
 
 
+def add():
+    ad = Add(Home)
+    Home.wait_window(ad.top)
 
+
+class Edit:
+    def __init__(self, parent):
+        top = self.top = Toplevel(parent)
+
+        Label(top,text="Edit").pack()
+
+        Label(top, text='What do you want to edit?')
+
+        cate = Button(top,fg='white',bg='blue' , text="Category", command=lambda: raise_frame(EditCategory))
+        cate.pack(pady=5, padx=5, side=LEFT)
+
+        activity = Button(top,fg='white',bg='blue' , text="Activity", command=lambda: raise_frame(EditActivity))
+        activity.pack(pady=5, padx=5, side=LEFT)
+
+
+def edit():
+    ed = Edit(Home)
+    Home.wait_window(ed.top)
 
 
 TopFrame = Frame(Home)
